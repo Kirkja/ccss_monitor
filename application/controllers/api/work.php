609f3835bd5e61e307cf30ee77a95de85a3df1b6
User1 <User1@tsc01>
2014-03-19 16:52:59 -0700

    Adding new code

--- application/controllers/api/work.php
+++ application/controllers/api/work.php
@@ -58,19 +58,22 @@ class Work extends CI_Controller {
         }
     }
 
+    
     public function getwork2() {
+        
+        $id = "802387cc-a583-11e3-aa5d-0014d16a86e4";
 
         $sql = "SELECT
-                BA.blockID, BA.assignedTO, BA.dueON,
+                BA.blockID, BA.assignedTO, DATE_FORMAT(BA.dueON, '%M %e, %Y') as dueON,
                 BB.label
                 FROM `block_assignment` AS BA
                 JOIN `login` AS LI ON LI.userID = BA.assignedTO
                 LEFT JOIN `block_bank` AS BB ON BB.id = BA.blockID
                 WHERE BA.active = 'y'
                 AND LI.userStatus = 'a'
-                AND LI.id = '802387cc-a583-11e3-aa5d-0014d16a86e4'
+                AND LI.id = '{$id}'
+                AND BA.completedON IS NULL 
                 ORDER BY BA.dueON, BB.label";
-
 
         $query = $this->db->query($sql);
 
@@ -119,10 +122,10 @@ class Work extends CI_Controller {
                         $c['imagePath'] = $blockB->samplePath;
                         $c['cashValue'] = $blockB->cashValue;  
                         if ($blockB->reviewedON) {
-                            $c['completed']  = 'y';
+                            $c['completed']  = 'completed';
                         }
                         else {
-                            $c['completed']  = 'n';
+                            $c['completed']  = '';
                         }
                         
                         $c['children'] = array();
@@ -148,6 +151,101 @@ class Work extends CI_Controller {
         }
     }
 
+    
+    
+    public function getwork3() {
+        
+        $id = "802387cc-a583-11e3-aa5d-0014d16a86e4";
+        $sql = "SELECT
+                BA.blockID, BA.assignedTO, DATE_FORMAT(BA.dueON, '%M %e, %Y') as dueON,
+                BB.label
+                FROM `block_assignment` AS BA
+                JOIN `login` AS LI ON LI.userID = BA.assignedTO
+                LEFT JOIN `block_bank` AS BB ON BB.id = BA.blockID
+                WHERE BA.active = 'y'
+                AND LI.userStatus = 'a'
+                AND LI.id = '{$id}'
+                AND BA.completedON IS NOT NULL 
+                ORDER BY BA.dueON, BB.label";
+
+        $query = $this->db->query($sql);
+
+        $data = array();
+
+        if ($query->num_rows() > 0) {
+
+            foreach ($query->result() as $block) {
+
+                $item['label']      = $block->label;
+                $item['id']         = $block->blockID;
+                $item['cashValue']  = 0.00;
+                $item['dueON']      = $block->dueON;
+                $item['children']   = array();
+               
+                $sqlB = "SELECT 
+                        samples.id AS id,
+                        samples.sampleName as label,
+                        samples.sampleImage,
+                        samples.samplePath,
+                        CASE samples.payRate
+                                WHEN 'A' THEN  0.50 * 2.50
+                                WHEN 'B' THEN  0.50 * 1.50
+                                WHEN 'C' THEN  0.50 * 1.00
+                                WHEN 'D' THEN  0.50 * 0.75
+                                WHEN 'E' THEN  0.50 * 0.50
+                                WHEN 'F' THEN  0.50 * 0.25
+                                ELSE 0.50
+                        END AS cashValue,
+                        SR.reviewedON
+                        FROM `block_samples` AS BS
+                        LEFT JOIN `sample_bank` AS samples ON samples.id = BS.sampleID
+                        LEFT JOIN sample_review SR ON (SR.blockID = BS.blockID AND SR.sampleID = BS.sampleID)
+                        WHERE BS.blockID = '{$block->blockID}'
+                        AND BS.active = 'y'";
+
+                $queryB = $this->db->query($sqlB);
+
+                if ($queryB->num_rows() > 0) {
+                   
+                   foreach ($queryB->result() as $blockB) {
+                        $c = array();
+                        $c['label'] = trim($blockB->label);
+                        $c['id'] = $blockB->id;
+                        $c['imageName'] = $blockB->sampleImage;
+                        $c['imagePath'] = $blockB->samplePath;
+                        $c['cashValue'] = $blockB->cashValue;  
+                        if ($blockB->reviewedON) {
+                            $c['completed']  = 'completed';
+                        }
+                        else {
+                            $c['completed']  = '';
+                        }
+                        
+                        $c['children'] = array();
+                                          
+                        $item['children'][] = $c;
+                        
+                        $item['cashValue'] += $blockB->cashValue;
+                    }
+                }
+                             
+                $data[] = $item;                                
+            }
+            
+            // Create the correct JSON payloads
+            $out = array('data'=>$data);
+            
+            // Set the correct JSON response header
+           header('Content-Type: application/json');
+           echo json_encode($out);          
+           
+           //  DEBUG ONLY
+           //echo "<pre>" . print_r($out, true) . "</pre>";
+        }
+    }
+    
+    
+    
     private function loadFolder($row) {
         $record = array();
         foreach ($row as $key => $value) {
--- application/views/workspace/tab_analysis.php
+++ application/views/workspace/tab_analysis.php
@@ -20,7 +20,7 @@
     <div class="ui-layout-center">
         <div class="ui-widget-header ui-corner-top">Center-Center</div>
         <div class="ui-widget-content container">
-            {{user.activeID}}
+            {{selected.id}}
             <div style="text-align:center" ng-show="selected.imagePath">
                 <img src="{{selected.imagePath}}{{selected.imageName}}" width="99%"/>
             </div>                    
@@ -36,19 +36,42 @@
             </div>
             <div class="ui-widget-content">
                 <treecontrol class="tree-classic" 
-                             tree-model="treedata2" 
+                             tree-model="openworkTree" 
                              node-children="children" 
                              on-selection="showSelected(node)" 
                              selected-node="node1">
                     {{node.label}}
-                </treecontrol>
+                </treecontrol>                
+
+            </div>
+            <div class="ui-state-default blockSummary">
+                <div ng-show="node1.dueON">
+                <span class="right">Due On {{node1.dueON}}</span>
+                ${{node1.cashValue}}
+                </div>
             </div>
         </div>
         <div class="ui-layout-south">
-            <div class="ui-widget-header ui-corner-top">Completed Work</div>
-            <div class="ui-widget-content">
-                West center stuff here
+            <div class="ui-widget-header ui-corner-top">Completed Work
+                <span class="btn right" ng-click="getClosedWork(user)">
+                    <img src="<?php echo base_url(); ?>css/images/refresh_btn_b.png" alt="Refresh Assignments" title="Refresh Assignments"/>
+                </span>             
             </div>
+            <div class="ui-widget-content">
+                <treecontrol class="tree-classic" 
+                             tree-model="closedworkTree" 
+                             node-children="children" 
+                             on-selection="showSelected(node)" 
+                             selected-node="node2">
+                    {{node.label}}
+                </treecontrol>
+            </div>
+            <div class="ui-state-default blockSummary">
+                <div ng-show="node2.dueON">
+                <span class="right">Due On {{node2.dueON}}</span>
+                ${{node2.cashValue}}
+                </div>
+            </div>            
         </div>
     </div>
 
